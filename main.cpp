434a2fdc5ee1de26d46e64261167dcaca1181edf
Alex Yang <aleozlx@gmail.com>
2019-08-21 00:37:55 -0500

    ..

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include <list>
 #include <vector>
 
-#define Cross2D(ax, ay, bx, by) ((ax) * (by) - (ay) * (bx))
-
 template<typename E>
 using Clipper = std::list<E>;
 
@@ -38,24 +36,25 @@ struct Vertex2D {
     }
 
     /// a x b = det(a b) = a1 * b2 - a2 * b1
-    T cross(Vertex2D<T> const &b) const {
-        return Cross2D(x, y, b.x, b.y);
+    T operator*(Vertex2D<T> const &b) const {
+        return x*b.y-y*b.x;
+    }
+
+    /// a x b = det(a b) = a1 * b2 - a2 * b1
+    Vertex2D<T> operator/(T normalizer) const {
+        return {x/normalizer, y/normalizer};
     }
 
     /// <0: left of the edge; >0: right of the edge; =0: on the edge
-    T cross(Vertex2D<T> const &q0, Vertex2D<T> const &q1) const {
-        return (q0-q1).cross(*this-q1);
+    // T cross(Vertex2D<T> const &q0, Vertex2D<T> const &q1) const {
+    //     return (q0-q1)*(*this-q1);
+    // }
+
+    bool operator<(typename Polygon<T>::Edge const &edge) const {
+        return (edge.p0-edge.p1)*(*this-edge.p1)<0;
     }
 
-    T cross(typename Polygon<T>::Edge const &edge) const {
-        return (edge.p0-edge.p1).cross(*this-edge.p1);
-    }
-
-    bool left_of(typename Polygon<T>::Edge const &edge) const {
-        return cross(edge)<0;
-    }
-
-    bool left_of(typename Box<T>::Edge const &edge) const {
+    bool operator<(typename Box<T>::Edge const &edge) const {
         switch(edge.side) {
             case Box<T>::Edge::Left: return x > edge.x;
             case Box<T>::Edge::Bottom: return y > edge.y;
@@ -65,6 +64,12 @@ struct Vertex2D {
     }
 };
 
+template<typename V>
+V intersect4p(V const &p0, V const &p1, V const &q0, V const &q1) {
+    V p = p0-p1, q = q0-q1, v = {p.x, q.x}, w = {p.y, q.y}, d = {p0*p1, q0*q1}, z = {d*v, d*w};
+    return z/(p*q);
+}
+
 template<typename T>
 struct Polygon {
     std::list<Vertex2D<T>> data;
@@ -73,11 +78,13 @@ struct Polygon {
 
     }
 
-    Polygon(Box<T> const &box) {
-        data.push_back({box.xmin, box.ymin});
-        data.push_back({box.xmax, box.ymin});
-        data.push_back({box.xmax, box.ymax});
-        data.push_back({box.xmin, box.ymax});
+    Polygon(Box<T> const &box): data({
+        {box.xmin, box.ymin},
+        {box.xmax, box.ymin},
+        {box.xmax, box.ymax},
+        {box.xmin, box.ymax}
+    }) {
+
     }
 
     void debug() {
@@ -90,19 +97,11 @@ struct Polygon {
     struct Edge {
         Vertex2D<T> p0, p1;
 
-        static Vertex2D<T> intersect(Vertex2D<T> const &p0, Vertex2D<T> const &p1, Vertex2D<T> const &q0, Vertex2D<T> const &q1) {
-            T dp = p0.cross(p1), dq = q0.cross(q1),
-                x = Cross2D(dp, dq, p0.x-p1.x, q0.x-q1.x),
-                y = Cross2D(dp, dq, p0.y-p1.y, q0.y-q1.y),
-                n = (p0-p1).cross(q0-q1);
-            return {x/n, y/n};
+        Vertex2D<T> operator*(Edge const &edge) {
+            return intersect4p(this->p0, this->p1, edge.p0, edge.p1);
         }
 
-        Vertex2D<T> intersect(Edge const &edge) {
-            return intersect(this->p0, this->p1, edge.p0, edge.p1);
-        }
-
-        Vertex2D<T> intersect(typename Box<T>::Edge const &edge) {
+        Vertex2D<T> operator*(typename Box<T>::Edge const &edge) {
             Vertex2D<T> d = p1-p0;
             switch(edge.side) {
                 case Box<T>::Edge::Top:
@@ -115,13 +114,17 @@ struct Polygon {
         }
     };
 
-    Clipper<Edge> asClipper() const {
+    std::list<Edge> GetEdges() const {
         Clipper<Edge> ret;
         for(auto c1 = data.begin(); c1!=data.end(); ++c1) {
             auto c0 = std::prev(c1 == data.begin()?data.end():c1);
             ret.push_back({*c0, *c1});
         }
         return ret;
+    }
+
+    Clipper<Edge> asClipper() const {
+        return GetEdges();
     }
 };
 
@@ -131,20 +134,20 @@ P SutherlandClipping(P const &polygon, C const &clipper) {
     for(auto clipper_edge: clipper) {
         auto pin = pout;
         pout.clear();
-        for(auto p1 = pin.begin(); p1!=pin.end(); ++p1) {
-            auto p0 = std::prev(p1 == pin.begin()?pin.end():p1);
-            typename P::Edge edge = {*p0, *p1};
-            if (p1->left_of(clipper_edge)) {
-                if (!(p0->left_of(clipper_edge)))
-                    pout.push_back(edge.intersect(clipper_edge));
-                pout.push_back(*p1);
+        for(auto edge: polygon.GetEdges()) {
+            if (edge.p1<clipper_edge) {
+                if (!(edge.p0<(clipper_edge)))
+                    pout.push_back(edge*clipper_edge);
+                pout.push_back(edge.p1);
             }
-            else if (p0->left_of(clipper_edge))
-                pout.push_back(edge.intersect(clipper_edge));
+            else if (edge.p0<clipper_edge)
+                pout.push_back(edge*clipper_edge);
         }
     }
     return P(pout);
 }
+
+// 2.5,2.5  2,2.5  1,2  2,1  2.5,1
 
 int main(int, char const **) {
     Box<float> box = {0,0,2.5,2.5};
